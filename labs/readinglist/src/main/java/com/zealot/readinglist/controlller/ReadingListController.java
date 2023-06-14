6dbf237c82a3c88b5d97b7aa1894ac9495e0e265
Jyoti Ranjan <zealot@pathfinder-2.local>
2023-06-14 12:11:46 +0530

    login page visible for readinglist application

---
+++ labs/readinglist/src/main/java/com/zealot/readinglist/controlller/ReadingListController.java
@@ -0,0 +1,37 @@
+package com.zealot.readinglist.controlller;
+import com.zealot.readinglist.database.Book;
+import com.zealot.readinglist.database.ReadingListRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import java.util.List;
+@Controller
+@RequestMapping("/")
+public class ReadingListController {
+    private ReadingListRepository readingListRepository;
+    @Autowired
+    public ReadingListController(
+            ReadingListRepository readingListRepository) {
+        this.readingListRepository = readingListRepository;
+    }
+    @RequestMapping(value="/{reader}", method=RequestMethod.GET)
+    public String readersBooks(
+            @PathVariable("reader") String reader,
+            Model model) {
+        List<Book> readingList =
+                readingListRepository.findByReader(reader);
+        if (readingList != null) {
+            model.addAttribute("books", readingList);
+        }
+        return "readingList";
+    }@RequestMapping(value="/{reader}", method=RequestMethod.POST)
+    public String addToReadingList(
+            @PathVariable("reader") String reader, Book book) {
+        book.setReader(reader);
+        readingListRepository.save(book);
+        return "redirect:/{reader}";
+    }
+}
