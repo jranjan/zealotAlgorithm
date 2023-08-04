2a25ad55f9320ab0219f5b059f27c02e29819f63
Jyoti Ranjan <zealot@pathfinder-2.local>
2023-08-04 12:15:34 +0530

    idiom repository is set

--- labs/idioms/src/main/java/org/zealot/collections/ArrayList.java
+++ labs/idioms/src/main/java/org/zealot/collections/ArrayList.java
@@ -1,2 +1,9 @@
-package org.zealot.collections;public class ArrayList {
+package org.zealot.collections;
+
+public class ArrayList {
+    public static void main(String[] args){
+
+        System.out.println("Hello, World!");
+
+    }
 }
